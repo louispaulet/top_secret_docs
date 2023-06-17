3efacc5dd4510b841f270bbf14729b7643d44bab
louispaulet <louispaulet13@gmail.com>
2023-06-17 20:02:27 +0200

    add even more property checks to ensure a title is found

--- main.js
+++ main.js
@@ -16,15 +16,14 @@ window.onload = function() {
             documentDropdown.onchange = function() {
                 const doc = documents[this.value];
                 documentDisplay.innerHTML = `
-                    <h2>${doc.subject ? doc.subject : 'N/A'}</h2>
+                    <h2>${doc.objectID ? doc.objectID : (doc.subject ? doc.subject : (doc.project ? doc.project : (doc.subjectID ? doc.subjectID : 'N/A')))}</h2>
                     <p><strong>Document ID:</strong> ${doc.docId ? doc.docId : 'N/A'}</p>
                     <p><strong>Classification:</strong> ${doc.classification ? doc.classification : 'N/A'}</p>
-                    <p><strong>Report Date:</strong> ${doc.reportDate ? doc.reportDate : 'N/A'}</p>
-                    <p><strong>Security Level:</strong> ${doc.securityLevel ? doc.securityLevel : 'N/A'}</p>
+                    <p><strong>Containment Procedures:</strong> ${doc.containmentProcedures ? doc.containmentProcedures : 'N/A'}</p>
+                    <p><strong>Description:</strong> ${doc.description ? doc.description : 'N/A'}</p>
+                    ${doc.procedures ? Object.entries(doc.procedures).map(([key, value]) => `<p><strong>${key}:</strong> ${value}</p>`).join('') : ''}
+                    <p><strong>Risk Code:</strong> ${doc.riskCode ? doc.riskCode : 'N/A'}</p>
                     <p><strong>Access Level:</strong> ${doc.accessLevel ? doc.accessLevel : 'N/A'}</p>
-                    <p><strong>Background:</strong> ${doc.background ? doc.background : 'N/A'}</p>
-                    ${doc.operationDetails ? `<p><strong>Operation Details:</strong> ${doc.operationDetails}</p>` : ''}
-                    ${doc.rehabilitationProtocol ? `<p><strong>Rehabilitation Protocol:</strong> ${doc.rehabilitationProtocol}</p>` : ''}
                     <p><strong>Addendums:</strong></p>
                     <ul>${doc.addendums && doc.addendums.length > 0 ? doc.addendums.map(addendum => `<li>${addendum}</li>`).join('') : '<li>N/A</li>'}</ul>
                 `;
