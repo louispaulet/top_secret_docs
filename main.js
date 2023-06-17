500737d69708c5e69fd252f44f3996fae97f93b6
louispaulet <louispaulet13@gmail.com>
2023-06-17 22:52:28 +0200

    add even more props

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ window.onload = function() {
                         <h2>${doc.objectID ? doc.objectID : (doc.subject ? doc.subject : (doc.project ? doc.project : (doc.subjectID ? doc.subjectID : 'N/A')))}</h2>
                         <p><strong>Document ID:</strong> ${doc.docId ? doc.docId : 'N/A'}</p>
                         <p><strong>Classification:</strong> ${doc.classification ? doc.classification : 'N/A'}</p>
-                        <p><strong>Containment Procedures:</strong> ${doc.containmentProcedures ? doc.containmentProcedures : (doc.reportSummary ? doc.reportSummary : 'N/A')}</p>
-                        <p><strong>Description:</strong> ${doc.description ? doc.description : (doc.details ? doc.details : 'N/A')}</p>
+                        <p><strong>Containment Procedures:</strong> ${doc.containmentProcedures ? doc.containmentProcedures : (doc.reportSummary ? doc.reportSummary : (doc.profile ? doc.profile : (doc.background ? doc.background : 'N/A')))}</p>
+                        <p><strong>Description:</strong> ${doc.description ? doc.description : (doc.details ? doc.details : (doc.currentActivities ? doc.currentActivities : (doc.evidence ? doc.evidence : (doc.analysis ? doc.analysis : (doc.operationDetails ? doc.operationDetails : (doc.rehabilitationProtocol ? doc.rehabilitationProtocol : 'N/A'))))))}</p>
                         ${doc.procedures ? Object.entries(doc.procedures).map(([key, value]) => `<p><strong>${key}:</strong> ${value}</p>`).join('') : ''}
                         <p><strong>Risk Code:</strong> ${doc.riskCode ? doc.riskCode : 'N/A'}</p>
                         <p><strong>Access Level:</strong> ${doc.accessLevel ? doc.accessLevel : 'N/A'}</p>
