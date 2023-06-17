d3dc03914309edbb5da8fc22d4146f82f3a5bdcc
louispaulet <louispaulet13@gmail.com>
2023-06-17 19:55:09 +0200

    more props

--- main.js
+++ main.js
@@ -16,17 +16,17 @@ window.onload = function() {
             documentDropdown.onchange = function() {
                 const doc = documents[this.value];
                 documentDisplay.innerHTML = `
-                    <h2>${doc.subject}</h2>
-                    <p><strong>Document ID:</strong> ${doc.docId}</p>
-                    <p><strong>Classification:</strong> ${doc.classification}</p>
-                    <p><strong>Report Date:</strong> ${doc.reportDate}</p>
-                    <p><strong>Security Level:</strong> ${doc.securityLevel}</p>
-                    <p><strong>Access Level:</strong> ${doc.accessLevel}</p>
-                    <p><strong>Background:</strong> ${doc.background}</p>
+                    <h2>${doc.subject ? doc.subject : 'N/A'}</h2>
+                    <p><strong>Document ID:</strong> ${doc.docId ? doc.docId : 'N/A'}</p>
+                    <p><strong>Classification:</strong> ${doc.classification ? doc.classification : 'N/A'}</p>
+                    <p><strong>Report Date:</strong> ${doc.reportDate ? doc.reportDate : 'N/A'}</p>
+                    <p><strong>Security Level:</strong> ${doc.securityLevel ? doc.securityLevel : 'N/A'}</p>
+                    <p><strong>Access Level:</strong> ${doc.accessLevel ? doc.accessLevel : 'N/A'}</p>
+                    <p><strong>Background:</strong> ${doc.background ? doc.background : 'N/A'}</p>
                     ${doc.operationDetails ? `<p><strong>Operation Details:</strong> ${doc.operationDetails}</p>` : ''}
                     ${doc.rehabilitationProtocol ? `<p><strong>Rehabilitation Protocol:</strong> ${doc.rehabilitationProtocol}</p>` : ''}
                     <p><strong>Addendums:</strong></p>
-                    <ul>${doc.addendums.map(addendum => `<li>${addendum}</li>`).join('')}</ul>
+                    <ul>${doc.addendums && doc.addendums.length > 0 ? doc.addendums.map(addendum => `<li>${addendum}</li>`).join('') : '<li>N/A</li>'}</ul>
                 `;
             };
 
